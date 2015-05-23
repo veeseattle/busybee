c31285a057faeb715137e77062b64787a8920aad
Vania <vania.baek@gmail.com>
2015-05-22 19:13:20 -0700

    Initial Commit

---
+++ Otto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Otto
+//
+//  Created by Vania Kurniawati on 5/22/15.
+//  Copyright (c) 2015 vivavania. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Otto/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Otto
+//
+//  Created by Vania Kurniawati on 5/22/15.
+//  Copyright (c) 2015 vivavania. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
