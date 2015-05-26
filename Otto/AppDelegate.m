8f763b192889d70c8f0621a03c0085b3a297b5b3
Vania <vania.baek@gmail.com>
2015-05-25 21:31:19 -0700

    tab bar controller not yet working

--- Otto/AppDelegate.m
+++ Otto/AppDelegate.m
@@ -27,26 +27,26 @@
 //  UIViewController  *rootController = [[UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]] instantiateViewControllerWithIdentifier:@"SEARCH_VC"];
 //  self.window.rootViewController = rootController;
 //  UITabBarController *tabBarController = (UITabBarController *)self.window.rootViewController;
-//  
-//  UITabBar *tabBar = tabBarController.tabBar;
-//  UITabBarItem *tabBarItem1 = [tabBar.items objectAtIndex:0];
-//  UITabBarItem *tabBarItem2 = [tabBar.items objectAtIndex:1];
-//  UITabBarItem *tabBarItem3 = [tabBar.items objectAtIndex:2];
-//  UITabBarItem *tabBarItem4 = [tabBar.items objectAtIndex:3];
-//  tabBar.translucent = NO;
-//  tabBar.barTintColor = [UIColor whiteColor];
-//  
-//  UIImage *image1 = [UIImage imageNamed:@"ottoactivity.png"];
-//  UIImage *image2 = [UIImage imageNamed:@"ottostart.png"];
-//  UIImage *image3 = [UIImage imageNamed:@"ottoid.png"];
-//  UIImage *image4 = [UIImage imageNamed:@"ottoaccident.png"];
-//  
-//  tabBarItem1 = [tabBarItem1 initWithTitle:@"Activity" image:image1 selectedImage:image1];
-//  tabBarItem2 = [tabBarItem1 initWithTitle:@"Start/Stop" image:image2 selectedImage:image2];
-//  tabBarItem3 = [tabBarItem1 initWithTitle:@"Card" image:image3 selectedImage:image3];
-//  tabBarItem4 = [tabBarItem1 initWithTitle:@"Accident" image:image4 selectedImage:image4];
-//  
-//  [self.window setRootViewController:tabBarController];
+ 
+  UITabBar *tabBar = [[UITabBar alloc] init];
+  UITabBarItem *tabBarItem1 = [tabBar.items objectAtIndex:0];
+  UITabBarItem *tabBarItem2 = [tabBar.items objectAtIndex:1];
+  UITabBarItem *tabBarItem3 = [tabBar.items objectAtIndex:2];
+  UITabBarItem *tabBarItem4 = [tabBar.items objectAtIndex:3];
+  tabBar.translucent = NO;
+  tabBar.barTintColor = [UIColor whiteColor];
+  
+  UIImage *image1 = [UIImage imageNamed:@"ottoactivity.png"];
+  UIImage *image2 = [UIImage imageNamed:@"ottostart.png"];
+  UIImage *image3 = [UIImage imageNamed:@"ottoid.png"];
+  UIImage *image4 = [UIImage imageNamed:@"ottoaccident.png"];
+  
+  tabBarItem1 = [tabBarItem1 initWithTitle:@"Activity" image:image1 selectedImage:image1];
+  tabBarItem2 = [tabBarItem1 initWithTitle:@"Start/Stop" image:image2 selectedImage:image2];
+  tabBarItem3 = [tabBarItem1 initWithTitle:@"Card" image:image3 selectedImage:image3];
+  tabBarItem4 = [tabBarItem1 initWithTitle:@"Accident" image:image4 selectedImage:image4];
+  
+ // [self.window setRootViewController:tabBarController];
   return YES;
 }
 
