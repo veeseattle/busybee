e359ca8954f55d1d39a2b0623d5855d1c4983ff7
Vania <vania.baek@gmail.com>
2015-06-06 23:04:00 -0700

    ok that annoying button doesn't show up anymore

--- Otto/ViewController.m
+++ Otto/ViewController.m
@@ -15,7 +15,6 @@
 #import <Parse/Parse.h>
 #import <ParseUI/ParseUI.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
-#import <FBSDKLoginKit/FBSDKLoginKit.h>
 #import <ParseFacebookUtilsV4/PFFacebookUtils.h>
 
 @interface ViewController () <UITableViewDataSource, UITableViewDelegate, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate, UIImagePickerControllerDelegate, UITextFieldDelegate>
